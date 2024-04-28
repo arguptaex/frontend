9da52fc429fad344d87f2317339d2efa34c9ca96
arguptaex <architgupta.rke@gmail.com>
2024-04-28 13:12:53 +0530

    project initialized

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ab
+          voluptatem aspernatur harum voluptate obcaecati similique pariatur,
+          distinctio libero! Natus quia ullam soluta.
+        </p>
+        <button>View Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+const Navbar = () => {
+  const [menu, setMenu] = useState("Home");
+
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("Home")}
+          className={menu == "Home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("Menu")}
+          className={menu == "Menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("About")}
+          className={menu == "About" ? "active" : ""}
+        >
+          About
+        </li>
+        <li
+          onClick={() => setMenu("Contact")}
+          className={menu == "Contact" ? "active" : ""}
+        >
+          Contact
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>SignIn</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
